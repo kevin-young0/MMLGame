8861d97b54d6184c62fb29a8a8846f22d6678653
unknown <speterson86@LAB-ECB176-14.cvtc.local>
2013-12-09 14:11:48 -0600

    images update

---
+++ MMLGame/src/mmlgame/sqlliteConnect.java
@@ -0,0 +1,37 @@
+/*
+ * This program allows user to create a file in a database, and access that file
+ *later in the future...
+ */
+package mmlgame;
+
+/**
+ *
+ * @author RNelson
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class sqlliteConnect {
+    
+    Connection conn= null;
+    
+    public static Connection ConnecrDb(){
+    
+        try{
+            
+        Class.forName("org.sqlite.JDBC");
+        Connection conn =DriverManager.getConnection("jdbc:sqlite:..\\MMLGameDB.sqlite");
+                //JOptionPane.showMessageDialog(null, "Connection not found");
+                return conn;
+                
+               }catch (Exception e) {
+                   
+                   JOptionPane.showMessageDialog(null, e);
+                   
+                return null;   
+               
+               }
+        
+        }
+            
+    }
